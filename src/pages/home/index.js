1a4f5676afc609f451aca58d597213f4f9c6e1e9
Jaleelk <abduljaleelkolamban@gmail.com>
2023-03-01 21:44:30 +0530

    api products

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const index = () => {
-  return <div>Bismillah. welcome to al muharami
-    <p>In progress</p>
-  </div>;
+  const fetchProducts = async () => {
+    const resProducts = await fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((products) => products);
+
+    console.log("Products", resProducts);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  return (
+    <div>
+      Bismillah. welcome to al muharami
+      <p>In progress</p>
+    </div>
+  );
 };
 
 export default index;
